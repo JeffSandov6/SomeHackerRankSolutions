f506b81941e9fdf462d2dd0ef1e6962dc9e816a3
Jeff <jeffrysandoval24@gmail.com>
2018-12-18 19:08:51 -0600

    Create Solution.java

---
+++ Algorithms/Greedy/Greedy Florist/Solution.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int getMinimumCost(int k, int[] c) {
+
+      int numFlowers = c.length - 1;
+      int minCost = 0;
+      int multBy = 1;
+      int personNum = k;
+
+      Arrays.sort(c);
+
+      while(numFlowers >= 0)
+      {
+        if(personNum == 0)
+        {
+          personNum = k;
+          multBy++;
+        }
+        minCost += c[numFlowers] * multBy;
+
+        personNum--;
+        numFlowers--;
+      }
+      return minCost;
+    }
+
+
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int minimumCost = getMinimumCost(k, c);
+
+        bufferedWriter.write(String.valueOf(minimumCost));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
